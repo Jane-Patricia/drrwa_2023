c6ca858e02ba626c716c14059cacc23a13626669
jane-patricia <72210472@students.ukdw.ac.id>
2023-05-15 13:14:24 +0700

    Pertemuan 10: Swashbuckle

---
+++ BookStoreApi_benar/Controllers/BooksController.cs
@@ -0,0 +1,130 @@
+using System.Net;
+using BookStoreApi.Models;
+using BookStoreApi.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookStoreApi.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class BooksController : ControllerBase
+{
+    private readonly BooksService _booksService;
+
+    public BooksController(BooksService booksService) =>
+        _booksService = booksService;
+
+    [HttpGet]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+    public async Task<List<Book>> Get() =>
+        await _booksService.GetAsync();
+
+    [HttpGet("{id:length(24)}")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+    public async Task<ActionResult<Book>> Get(string id)
+    {
+        var book = await _booksService.GetAsync(id);
+
+        if (book is null)
+        {
+            return NotFound();
+        }
+
+        return book;
+    }
+
+    // public HttpResponseMessage Post(Book book)
+    //     {
+    //         if (ModelState.IsValid)
+    //         {
+    //             // Do something with the product (not shown).
+
+    //             return new HttpResponseMessage(HttpStatusCode.OK);
+    //         }
+    //         else
+    //         {
+    //             return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ModelState);
+    //         }
+    //     }
+
+    /// <summary>
+    /// Creates a TodoItem.
+    /// </summary>
+    /// <param name="item"></param>
+    /// <returns>A newly created TodoItem</returns>
+    /// <remarks>
+    /// Sample request:
+    ///
+    ///     POST /Todo
+    ///     {
+    ///        "id": 1,
+    ///        "name": "Item #1",
+    ///        "isComplete": true
+    ///     }
+    ///
+    /// </remarks>
+    /// <response code="201">Returns the newly created item</response>
+    /// <response code="400">If the item is null</response>
+    [HttpPost]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+    public async Task<IActionResult> Post(Book newBook)
+    {
+        await _booksService.CreateAsync(newBook);
+
+        return CreatedAtAction(nameof(Get), new { id = newBook.Id }, newBook);
+    }
+
+    [HttpPut("{id:length(24)}")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+    public async Task<IActionResult> Update(string id, Book updatedBook)
+    {
+        var book = await _booksService.GetAsync(id);
+
+        if (book is null)
+        {
+            return NotFound();
+        }
+
+        updatedBook.Id = book.Id;
+
+        await _booksService.UpdateAsync(id, updatedBook);
+
+        return NoContent();
+    }
+
+    [HttpDelete("{id:length(24)}")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+    public async Task<IActionResult> Delete(string id)
+    {
+        var book = await _booksService.GetAsync(id);
+
+        if (book is null)
+        {
+            return NotFound();
+        }
+
+        await _booksService.RemoveAsync(id);
+
+        return NoContent();
+    }
+}
